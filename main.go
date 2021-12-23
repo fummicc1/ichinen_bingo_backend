00da8611855a3829e9b0cd87458c387fa7e4b538
onyanko-pon <engineer.tsukasa@gmail.com>
2021-12-23 20:20:25 +0900

    add main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(
+		e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))),
+	)
+}
