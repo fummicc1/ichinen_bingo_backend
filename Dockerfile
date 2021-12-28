59bb4122585f795f0670f84bbb34b76d6fb77907
onyanko-pon <engineer.tsukasa@gmail.com>
2021-12-28 16:41:43 +0900

    create endpoint

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM golang:1.17 as builder
+
+WORKDIR /
+RUN mkdir /app
+COPY . /app/
+
+WORKDIR /app
+
+# RUN go mod tidy
+RUN go mod download
+
+ARG CGO_ENABLED=0
+ARG GOOS=linux
+ARG GOARCH=amd64
+WORKDIR /app
+RUN go build \
+  -o /go/bin/main
+
+
+FROM alpine:3.12 as runner
+
+COPY --from=builder /go/bin/main /main
+
+CMD /main
--- handler/bingo.go
+++ handler/bingo.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/onyanko-pon/ichinen-bingo/entity"
 	"github.com/onyanko-pon/ichinen-bingo/repository"
 )
 
@@ -24,6 +25,39 @@ func (handler BingoHandler) GetBingo(ctx echo.Context) error {
 	id, _ := strconv.Atoi(idStr)
 
 	bingo, _ := handler.bingoRepository.GetBingo(ctx.Request().Context(), uint64(id))
+	return ctx.JSON(http.StatusOK, echo.Map{
+		"bingo": bingo,
+	})
+}
+
+type requestBodyCreateBingo struct {
+	Todos []string `json:"todos"`
+	Title string   `json:"title"`
+}
+
+func (handler BingoHandler) CreateBingo(ctx echo.Context) error {
+
+	requestBody := new(requestBodyCreateBingo)
+	if err := ctx.Bind(requestBody); err != nil {
+		return err
+	}
+
+	if len(requestBody.Todos) != 25 {
+		return ctx.JSON(http.StatusBadRequest, echo.Map{
+			"message": "invalid todo length",
+		})
+	}
+
+	var todoList entity.TodoList
+	for i, todoTitle := range requestBody.Todos {
+		todoList.Todos[i] = entity.Todo{
+			Title:       todoTitle,
+			IsCompleted: false,
+			Index:       uint64(i),
+		}
+	}
+
+	bingo, _ := handler.bingoRepository.Create(ctx.Request().Context(), requestBody.Title, todoList)
 	return ctx.JSON(http.StatusOK, echo.Map{
 		"bingo": bingo,
 	})
